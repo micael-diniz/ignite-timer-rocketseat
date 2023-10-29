c59dc41a654dc603a71a7d2e9f98566effc99a6d
Micael Diniz <micaeldiniz6@gmail.com>
2023-10-29 15:34:30 -0300

    feat: improving inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,13 +16,27 @@ export function Home() {
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Study Next.js" />
+            <option value="Study Nest.js" />
+            <option value="Practice pico8" />
+            <option value="Nothing" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
