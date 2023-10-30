517fd9d4ed376b3cb882edcdf0d3dd9a29734db9
Micael Diniz <micaeldiniz6@gmail.com>
2023-10-30 09:11:24 -0300

    feat(Application features): Reducing countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,8 +12,8 @@ import {
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
 import { useForm } from 'react-hook-form'
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import { differenceInSeconds } from 'date-fns'
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
   minutesAmount: zod
@@ -28,6 +28,7 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 
 export function Home() {
@@ -50,6 +51,7 @@ export function Home() {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     }
 
     setCycles((state) => [...state, newCycle])
@@ -57,6 +59,16 @@ export function Home() {
     reset()
   }
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        )
+      }, 1000)
+    }
+  }, [activeCycle])
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
